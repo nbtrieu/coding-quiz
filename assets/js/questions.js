05731457010e09af27e6191541ac17e522c9f3d9
Nicole T <nbtrieu@uci.edu>
2022-11-14 22:54:41 -0800

    added js code to start quiz and show questions upon click events

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,27 +2,27 @@
 let questions = [
     {
         prompt: 'Which of the following is NOT a primitive data type in JavaScript?',
-        choices: ['Booleans', 'Null', 'Strings', 'Lists'],
+        choice: ['Booleans', 'Null', 'Strings', 'Lists'],
         answer: 'Lists',
     },
     {
         prompt: 'Hoisting is the default behavior of JavaScript where all the variable and function declarations are moved to the _____',
-        choices: ['top', 'bottom', 'center', 'end'],
+        choice: ['top', 'bottom', 'center', 'end'],
         answer: 'top',
     },
     {
         prompt: 'Which operator is used to compare both values and types?',
-        choices: ['==', '===', '>=', '='],
+        choice: ['==', '===', '>=', '='],
         answer: '===',
     },
     {
         prompt: 'Which keyword in an object method refers to the object itself?',
-        choices: ['object', 'it', 'this', 'self'],
+        choice: ['object', 'it', 'this', 'self'],
         answer: 'this',
     },
     {
         prompt: '___ is an application programming interface for HTML documents that allows documents to be accessed and manipulated.',
-        choices: ['DOC', 'DOM', 'API', 'Developer Tools'],
+        choice: ['DOC', 'DOM', 'API', 'Developer Tools'],
         answer: 'DOM',
     }
 ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,68 @@
-let currentQuestionIndex = 0;
-let time = questions.length * 10;
-let timer;
+let currentQuestionIndex = 0; // starting at the element at index=0 in the question array
+let timeLeft = 75; // questions.length * 10;
+let timeInterval;
+
+let timeElement = document.querySelector("#time-span");
+let startButton = document.querySelector("#start-button");
+let questionsElement = document.querySelector("#questions");
+let questionPromptElement = document.querySelector("#question-prompt"); // can this NOT be global??
+let choicesElement = document.querySelector("#choices");
+
+console.log(startButton);
+startButton.addEventListener('click', startQuiz);
+
+function startQuiz() {
+    let startScreenElement = document.querySelector("#start-screen");
+    // hide start screen
+    startScreenElement.setAttribute("class", "hide");
+
+    // unhide questions
+    questionsElement.removeAttribute("class");
+
+    // show starting time
+    timeElement.textContent = timeLeft;
+
+    // start timer
+    timeInterval = setInterval(function() {
+        timeLeft--;
+
+        if (timeLeft >= 1) {
+            timeElement.textContent = timeLeft;
+        } else if (timeLeft <= 0) {
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+
+    // show questions and answer choices
+    getQuestion();
+}
+
+function getQuestion() {
+    // get each question from array of question objects in questions.js
+    let currentQuestion = questions[currentQuestionIndex];
+    console.log(currentQuestion);
+
+    // question prompt h2 element is blank right now so we need to add text to it
+    questionPromptElement.textContent = currentQuestion.prompt;
+
+    // clear out old question choices
+    choicesElement.innerHTML = "";
+
+    // loop over each choice to create choice buttons with text
+    let choice = currentQuestion.choice;
+    for (let i = 0; i < choice.length; i++) {
+        let choiceButton = document.createElement("button");
+        choiceButton.setAttribute("class", "choices");
+        choiceButton.innerHTML = choice[i];
+        choicesElement.appendChild(choiceButton);
+        
+        console.log(choiceButton);
+        choiceButton.addEventListener('click', getQuestion);
+    }
+
+    currentQuestionIndex++;
+}
+
+function subtractTime() {
+    
+}
